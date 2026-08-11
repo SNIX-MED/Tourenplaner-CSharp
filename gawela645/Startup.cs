8655303e6bef1a9bcf3126448ed0ca0764b5ae1e
SNIX-MED <sminedog@gmail.com>
2026-08-11 10:37:18 +0200

    Register central RAL palette store

---
+++ gawela645/Startup.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Smartstore.Engine;
+using Smartstore.Engine.Builders;
+using Smartstore.Web.Controllers;
+using Gawela.ColorConfigurator.Filters;
+using Gawela.ColorConfigurator.Services;
+
+namespace Gawela.ColorConfigurator;
+
+internal class Startup : StarterBase
+{
+    public override bool Matches(IApplicationContext appContext)
+        => appContext.IsInstalled;
+
+    public override void ConfigureServices(IServiceCollection services, IApplicationContext appContext)
+    {
+        services.Configure<MvcOptions>(o =>
+        {
+            o.Filters.AddEndpointFilter<GawelaProductDetailFilter, ProductController>()
+                .ForAction(x => x.ProductDetails(0, null))
+                .WhenNonAjax();
+        });
+
+        services.AddSingleton<GawelaAssetStore>();
+        services.AddSingleton<GawelaPaletteStore>();
+    }
+}
